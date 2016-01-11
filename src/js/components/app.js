a3804d74eb42dc27661728ab46b3b897b7e81121
Jonathan Sevold <jmsevold@gmail.com>
2016-01-11 13:28:56 -0800

    actions done

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import AppActions from '../action/app-actions'
 
 export default class App extends React.Component{
   render(){
-    return <h1>Flux</h1>
+    return <h1 onClick={AppActions.addItem.bind(null,'item')}>Flux</h1>
   
   }
 }
